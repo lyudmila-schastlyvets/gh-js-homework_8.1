745490c124857cdaf73b336d8793babebcc8d393
liudmyla_schastlyvets <lyudmila.zoria@n3wnormal.com>
2017-12-07 19:10:07 +0200

    Added ability to move element

--- js/main.js
+++ js/main.js
@@ -27,3 +27,58 @@ document.getElementById('box').addEventListener("click", function(event){
             .removeChild(test.parentNode);
     }
 }, false);
+
+var mydragg = function(){
+    return {
+        move : function(divid,xpos,ypos){
+            divid.style.left = xpos + 'px';
+            divid.style.top = ypos + 'px';
+        },
+        startMoving : function(divid,container,evt){
+            evt = evt || window.event;
+            console.log(divid.style.top);
+            var posX = evt.clientX,
+                posY = evt.clientY,
+                divTop = divid.style.top,
+                divLeft = divid.style.left,
+                eWi = parseInt(divid.style.width),
+                eHe = parseInt(divid.style.height),
+                cWi = parseInt(document.getElementById(container).style.width),
+                cHe = parseInt(document.getElementById(container).style.height);
+            console.log(divid.style.top);
+            document.getElementById(container).style.cursor='move';
+            divTop = divTop.replace('px','');
+            divLeft = divLeft.replace('px','');
+            var diffX = posX - divLeft,
+                diffY = posY - divTop;
+            document.onmousemove = function(evt){
+                evt = evt || window.event;
+                var posX = evt.clientX,
+                    posY = evt.clientY,
+                    aX = posX - diffX,
+                    aY = posY - diffY;
+                if (aX < 0) aX = 0;
+                if (aY < 0) aY = 0;
+                if (aX + eWi > cWi) aX = cWi - eWi;
+                if (aY + eHe > cHe) aY = cHe -eHe;
+                mydragg.move(divid,aX,aY);
+            }
+        },
+        stopMoving : function(container){
+            var a = document.createElement('script');
+            document.getElementById(container).style.cursor='default';
+            document.onmousemove = function(){}
+        },
+    }
+}();
+
+document.getElementById('item').addEventListener('mousedown', function(event) {
+    mydragg.startMoving(this,"box",event);
+});
+
+document.getElementById('item').addEventListener('mouseup', function() {
+    mydragg.stopMoving("box");
+});
+
+console.dir(document.getElementById('box'));
+
